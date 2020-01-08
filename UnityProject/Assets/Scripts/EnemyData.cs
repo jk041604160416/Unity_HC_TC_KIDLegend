81e9b596d4abbcf1eb6bd82fbbb63a1069e6be36
jk041604160416 <57181323+jk041604160416@users.noreply.github.com>
2020-01-08 21:35:10 +0800

    1/8

---
+++ UnityProject/Assets/Scripts/EnemyData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+// 腳本化物件 ScriptableObject:將此腳本的資料儲存為物件並放在 project 內
+
+[CreateAssetMenu(fileName ="怪物資料",menuName ="JK/怪物")]
+public class EnemyData : ScriptableObject
+{
+    
+}
